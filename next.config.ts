ab71ef7ad25a12bc7256b19207ffc67cf1df58ce
Anand Singh Chauhan <anandsinghchauhan03646@gmail.com>
2026-01-12 23:15:20 +0530

    edit pages next config for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "fakestoreapi.com",
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
         pathname: "/img/**",
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
